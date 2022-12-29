dfc404d3041398a929ea8376219049f95b5cc7a7
Вадим Арбузов <www.vad.a@mail.ru>
2022-12-29 15:59:30 +0300

    added empty cells list

--- ConsoleSnakeMark2/GameGrid.cs
+++ ConsoleSnakeMark2/GameGrid.cs
@@ -1,22 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace ConsoleSnakeMark2 {
     public class GameGrid {
         readonly ICell[,] gameGrid;
+        readonly HashSetIndexed emptyCells;
         Point currentSnakeHead;
         Point currentSnakeTail;
 
         public GameGrid(int height, int width) {
             gameGrid = new ICell[height, width];
             //grid init
+            //add empty cells
         }
-        
+
         public ICell this[Point point] => gameGrid[point.X, point.Y];
 
         void SetItem(Point point, ICell item) {
             gameGrid[point.X, point.Y] = item;
+            emptyCells.Remove(point);
+        }
+
+        void ReleaseItem(Point point) {
+            gameGrid[point.X, point.Y] = new EmptyCell();
+            emptyCells.Add(point);
         }
 
         public void UpdateSnakeHead(Point newHead) {
@@ -26,7 +36,7 @@ namespace ConsoleSnakeMark2 {
         }
 
         public void UpdateSnakeTail(Point newTail) {
-            SetItem(currentSnakeTail, new EmptyCell());
+            ReleaseItem(currentSnakeTail);
             if (newTail != currentSnakeHead)
                 SetItem(newTail, new MovingSnakeTailCell());
             currentSnakeTail = newTail;
@@ -38,6 +48,32 @@ namespace ConsoleSnakeMark2 {
 
         public void AddFood(Point point, int foodValue) {
             SetItem(point, new FoodCell(foodValue));
+        }
+    }
+
+    public class HashSetIndexed {
+        readonly HashSet<Point> points;
+
+        public Point this[int index] {
+            get {
+                var enumerator = points.GetEnumerator();
+                for (int i = 0; i < index; i++) 
+                    enumerator.MoveNext();
+                return enumerator.Current;
+            }
+        }
+        public int Count => points.Count;
+
+        public HashSetIndexed() {
+            points = new HashSet<Point>();
+        }
+
+        public bool Add(Point point) {
+            return points.Add(point);
+        }
+
+        public bool Remove(Point point) {
+            return points.Remove(point);
         }
     }
 }
--- ConsoleSnakeMark2/Point.cs
+++ ConsoleSnakeMark2/Point.cs
@@ -1,6 +1,11 @@
-﻿namespace ConsoleSnakeMark2 {
-    public class Point {
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleSnakeMark2 {
+    public class Point : IEquatable<Point> {
         public static bool operator ==(Point point1, Point point2) {
+            if (point1 == null || point2 == null)
+                return false;
             return point1.X == point2.X && point1.Y == point2.Y;
         }
 
@@ -14,6 +19,21 @@
         public Point(int x, int y) {
             X = x;
             Y = y;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(X, Y);
+        }
+
+        public bool Equals([AllowNull] Point other) {
+            return this == other;
+        }
+
+        public override bool Equals(object obj) {
+            var point = obj as Point;
+            if (point == null)
+                return false;
+            return this == point;
         }
     }
 
