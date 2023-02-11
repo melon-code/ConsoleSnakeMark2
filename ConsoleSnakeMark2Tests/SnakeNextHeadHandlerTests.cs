7a3de62b0af575a13100163c958515609f030aa3
Вадим Арбузов <www.vad.a@mail.ru>
2023-02-11 18:57:16 +0300

    snake head handler tests

---
+++ ConsoleSnakeMark2Tests/SnakeNextHeadHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ConsoleSnakeMark2;
+
+namespace ConsoleSnakeMark2Tests {
+    class SnakeNextHeadHandlerTests {
+        static SnakePoint InitialPoint => new SnakePoint(2, 2);
+        static SnakePoint NextPoint => InitialPoint.GetNextSnakePoint(Direction.Right);
+
+        static SnakeNextHeadHandler CreateHeadHandler() {
+            return new SnakeNextHeadHandler(new SnakeList(InitialPoint));
+        }
+        
+        static void IsOppositeTest(bool expected, Direction direction1, Direction direction2) {
+            const string methodName = "IsOpposite";
+            Assert.AreEqual(expected, TestExtensionMethods.InvokeStaticMethod<bool>(typeof(SnakeNextHeadHandler), methodName, direction1, direction2));
+        }
+        
+        [TestCase(Direction.Left, Direction.Right)]
+        [TestCase(Direction.Up, Direction.Down)]
+        public void IsOppositeTrueTest(Direction direction1, Direction direction2) {
+            IsOppositeTest(true, direction1, direction2);
+        }
+
+        [TestCase(Direction.Left, Direction.Up)]
+        [TestCase(Direction.Left, Direction.Down)]
+        [TestCase(Direction.Right, Direction.Up)]
+        [TestCase(Direction.Right, Direction.Up)]
+        public void IsOppositeFalseTest(Direction direction1, Direction direction2) {
+            IsOppositeTest(false, direction1, direction2);
+        }
+
+        [TestCase(Direction.Right, Direction.Left)]
+        [TestCase(Direction.Up, Direction.Up)]
+        public void SetNewHeadDirectionTest(Direction expected, Direction setDirection) {
+            const string name = "currentDirection";
+            var handler = CreateHeadHandler();
+            handler.SetNewHeadDirection(setDirection);
+            Assert.AreEqual(expected, handler.GetFieldValue<Direction>(name));
+        }
+
+        [Test]
+        public void SetTeleportedHeadTest() {
+            const string boolName = "isTeleported";
+            const string pointName = "teleportedHead";
+            Point teleportedHead = new Point(4, 6);
+            var handler = CreateHeadHandler();
+            handler.SetTeleportedHead(teleportedHead);
+            Assert.IsTrue(handler.GetFieldValue<bool>(boolName));
+            Assert.AreEqual(teleportedHead, handler.GetFieldValue<Point>(pointName));
+        }
+
+        [Test]
+        public void GetNextHeadTest() {
+            var handler = CreateHeadHandler();
+            Assert.AreEqual(NextPoint, handler.GetNextHead());
+        }
+
+        [Test]
+        public void GetNextHeadTeleportedTest() {
+            Point teleported = new Point(2, 6);
+            var handler = CreateHeadHandler();
+            handler.SetTeleportedHead(teleported);
+            Assert.AreEqual(teleported, handler.GetNextHead());
+            Assert.AreEqual(NextPoint, handler.GetNextHead());
+        }
+    }
+}
--- ConsoleSnakeMark2Tests/TestExtensionMethods.cs
+++ ConsoleSnakeMark2Tests/TestExtensionMethods.cs
@@ -1,15 +1,26 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace ConsoleSnakeMark2Tests {
     public static class TestExtensionMethods {
-        public static T GetFieldValue<T>(this object sut, string name) {
-            var field = sut.GetType().GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            return (T)field?.GetValue(sut);
+        public static T GetFieldValue<T>(this object testObject, string name) {
+            var field = testObject.GetType().GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            return (T)field?.GetValue(testObject);
         }
 
-        public static T GetPropertyValue<T>(this object sut, string name) {
-            var field = sut.GetType().GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            return (T)field?.GetValue(sut);
+        public static T GetPropertyValue<T>(this object testObject, string name) {
+            var field = testObject.GetType().GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            return (T)field?.GetValue(testObject);
+        }
+
+        public static T InvokeMethod<T>(this object testObject, string name, params object[] parameters) {
+            var method = testObject.GetType().GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            return (T)method?.Invoke(testObject, parameters);
+        }
+
+        public static T InvokeStaticMethod<T>(Type testObjectType, string name, params object[] parameters) {
+            var method = testObjectType.GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            return (T)method?.Invoke(null, parameters);
         }
     }
 }
